9002628b755644106a54d76f01ff9851c400e7a1
Anna <44573387+annfike@users.noreply.github.com>
2021-10-13 10:24:28 +0300

    Initial commit

---
+++ load_books.py
@@ -0,0 +1,33 @@
+import requests
+import pathlib
+from pathlib import Path
+
+# url = "https://tululu.org/txt.php?id=32168"
+#
+# response = requests.get(url)
+# response.raise_for_status()
+#
+# filename = 'sand_of_mars.txt'
+# with open(filename, 'wb') as file:
+#     file.write(response.content)
+
+
+def load_book(path, filename, url, params=None):
+    filepath = Path(path) / filename
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with filepath.open('wb') as file:
+        file.write(response.content)
+
+
+path = 'books'
+pathlib.Path(path).mkdir(exist_ok=True)
+for index in range(10):
+    url = "https://tululu.org/txt.php"
+    payload = {"id": index}
+    filename = f'book{index}.txt'
+    filepath = Path(path) / filename
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    with filepath.open('wb') as file:
+        file.write(response.content)
