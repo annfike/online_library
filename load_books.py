fe4256a9ef1f5f57b9576d115407e946f415ffb2
Anna <44573387+annfike@users.noreply.github.com>
2021-10-13 14:27:26 +0300

    download books with titles

--- load_books.py
+++ load_books.py
@@ -1,33 +1,47 @@
 import requests
 import pathlib
 from pathlib import Path
-
-# url = "https://tululu.org/txt.php?id=32168"
-#
-# response = requests.get(url)
-# response.raise_for_status()
-#
-# filename = 'sand_of_mars.txt'
-# with open(filename, 'wb') as file:
-#     file.write(response.content)
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 
-def load_book(path, filename, url, params=None):
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def download_txt(url, filename, path, params=None):
+    filename = sanitize_filename(filename)
     filepath = Path(path) / filename
     response = requests.get(url, params=params)
     response.raise_for_status()
+    check_for_redirect(response)
     with filepath.open('wb') as file:
         file.write(response.content)
+
+
+def get_book_title():
+    url = f'https://tululu.org/b{index}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('body').find('h1')
+    title_text = title_tag.text.split('::')
+    title_text = title_text[0].strip()
+    return title_text
 
 
 path = 'books'
 pathlib.Path(path).mkdir(exist_ok=True)
 for index in range(1,11):
-    url = "https://tululu.org/txt.php"
     payload = {"id": index}
-    filename = f'book{index}.txt'
+    filename = get_book_title()
+    filename = f'{index}.{filename}.txt'
     filepath = Path(path) / filename
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    with filepath.open('wb') as file:
-        file.write(response.content)
+    try:
+        download_txt("https://tululu.org/txt.php", filename, path, params=payload)
+    except requests.HTTPError:
+        pass
+
+
+
