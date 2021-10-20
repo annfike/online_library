de963c90e7c6c9f909066afc678c8b20c070d290
Anna <44573387+annfike@users.noreply.github.com>
2021-10-20 15:45:30 +0300

    add more args

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -33,7 +33,7 @@ def download_txt(url, filename, books_path, params=None):
     filepath = Path(books_path) / filename
     response = requests.get(url, params=params)
     response.raise_for_status()
-    with filepath.open('w', encoding="utf-8") as file:
+    with filepath.open('w', encoding='utf-8') as file:
         file.write(response.text)
 
 
@@ -55,17 +55,17 @@ def parse_book_page(index):
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    title_selector = "body h1"
+    title_selector = 'body h1'
     title_tag = soup.select_one(title_selector)
     title, author = title_tag.text.split('::')
     title, author = title.strip(), author.strip()
-    pic_selector = ".bookimage img"
+    pic_selector = '.bookimage img'
     pic = soup.select_one(pic_selector)['src']
     pic_link = urljoin('https://tululu.org', pic)
-    genre_selector = "span.d_book a"
+    genre_selector = 'span.d_book a'
     genres_tag = soup.select(genre_selector)
     genres = [genre.text for genre in genres_tag]
-    comments_selector = ".texts span"
+    comments_selector = '.texts span'
     comments_tag = soup.select(comments_selector)
     comments = [comment.text for comment in comments_tag]
     book = {
@@ -79,14 +79,22 @@ def parse_book_page(index):
 
 
 def main():
-    books_path = 'books'
-    images_path = 'images'
+    parser = argparse.ArgumentParser(description='Get book')
+    parser.add_argument('--start_page', default=1, type=int, help='Начальная страница')
+    parser.add_argument('--end_page', default=701, type=int, help='Конечная страница')
+    parser.add_argument('--dest_folder', type=str, help='Папка для скачивания книг и картинок')
+    parser.add_argument('--skip_imgs', action='store_true', help='Не скачивать картинки')
+    parser.add_argument('--skip_txt', action='store_true', help='Не скачивать книги')
+    parser.add_argument('--json_path', type=str, help='path to JSON')
+    args = parser.parse_args()
+    if args.dest_folder:
+        books_path = args.dest_folder
+        images_path = args.dest_folder
+    else:
+        books_path = 'books'
+        images_path = 'images'
     pathlib.Path(books_path).mkdir(exist_ok=True)
     pathlib.Path(images_path).mkdir(exist_ok=True)
-    parser = argparse.ArgumentParser(description='Get book')
-    parser.add_argument('--start_page', default=1, type=int, help='Укажите начальную страницу')
-    parser.add_argument('--end_page', default=701, type=int, help='Укажите конечную страницу')
-    args = parser.parse_args()
     book_links = parse_categorye(args.start_page, args.end_page)
     for book_link in book_links:
         parsed = urlsplit(book_link)
@@ -94,13 +102,21 @@ def main():
         payload = {'id': book_id}
         try:
             book_page = parse_book_page(book_id)
-            with open("books.json", "a") as my_file:
+            if args.json_path:
+                json_path = args.json_path
+            else:
+                json_path = ''
+            pathlib.Path(json_path).mkdir(exist_ok=True)
+            filepath_json = Path(json_path) / 'books.json'
+            with open(filepath_json, 'a') as my_file:
                 json.dump(book_page, my_file, ensure_ascii=False)
             book_title = book_page['Заголовок']
             filename = f'{book_id}.{book_title}.txt'
             url = book_page['Картинка']
-            download_image(url, book_id, images_path)
-            download_txt('https://tululu.org/txt.php', filename, books_path, params=payload)
+            if args.skip_imgs == False:
+                download_image(url, book_id, images_path)
+            if args.skip_txt == False:
+                download_txt('https://tululu.org/txt.php', filename, books_path, params=payload)
         except requests.HTTPError:
             pass
 
