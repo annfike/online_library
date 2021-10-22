332aca29f04b84140cb0d84f0c537728e5b7868e
Anna <44573387+annfike@users.noreply.github.com>
2021-10-22 18:06:41 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,24 +4,33 @@ import pathlib
 from pathlib import Path
 from urllib.parse import unquote, urljoin, urlsplit
 import json
+import re
 
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 
-def parse_categorye(start_page, end_page):
+def parse_category(start_page, end_page):
     for page in range(start_page, end_page):
         url = f'https://tululu.org/l55/{page}'
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'lxml')
-        book_selector = '.bookimage a'
-        book_tags = soup.select(book_selector)
+        book_tags = soup.select('.bookimage a')
         books = [book['href'] for book in book_tags]
-        book_pages = [urljoin('https://tululu.org', book) for book in books]
-        return book_pages
+    return books
+
         
+def get_category_last_page():
+    url = 'https://tululu.org/l55/1'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    last_page_tags = soup.select('.npage')[-1]
+    last_page = last_page_tags.text
+    return last_page
+
 
 def check_for_redirect(response):
     if response.history:
@@ -55,18 +64,14 @@ def parse_book_page(index):
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    title_selector = 'body h1'
-    title_tag = soup.select_one(title_selector)
+    title_tag = soup.select_one('body h1')
     title, author = title_tag.text.split('::')
     title, author = title.strip(), author.strip()
-    pic_selector = '.bookimage img'
-    pic = soup.select_one(pic_selector)['src']
+    pic = soup.select_one('.bookimage img')['src']
     pic_link = urljoin('https://tululu.org', pic)
-    genre_selector = 'span.d_book a'
-    genres_tag = soup.select(genre_selector)
+    genres_tag = soup.select('span.d_book a')
     genres = [genre.text for genre in genres_tag]
-    comments_selector = '.texts span'
-    comments_tag = soup.select(comments_selector)
+    comments_tag = soup.select('.texts span')
     comments = [comment.text for comment in comments_tag]
     book = {
         'Заголовок': title,
@@ -81,44 +86,43 @@ def parse_book_page(index):
 def main():
     parser = argparse.ArgumentParser(description='Get book')
     parser.add_argument('--start_page', default=1, type=int, help='Начальная страница')
-    parser.add_argument('--end_page', default=701, type=int, help='Конечная страница')
-    parser.add_argument('--dest_folder', type=str, help='Папка для скачивания книг и картинок')
+    parser.add_argument('--end_page', default=get_category_last_page(), type=int, help='Конечная страница')
+    parser.add_argument('--book_folder', default='books', type=str, help='Папка для скачивания книг')
+    parser.add_argument('--pic_folder', default='images', type=str, help='Папка для скачивания картинок')
     parser.add_argument('--skip_imgs', action='store_true', help='Не скачивать картинки')
     parser.add_argument('--skip_txt', action='store_true', help='Не скачивать книги')
     parser.add_argument('--json_path', type=str, help='path to JSON')
     args = parser.parse_args()
-    if args.dest_folder:
-        books_path = args.dest_folder
-        images_path = args.dest_folder
-    else:
-        books_path = 'books'
-        images_path = 'images'
+    books_path = args.book_folder
+    images_path = args.pic_folder
     pathlib.Path(books_path).mkdir(exist_ok=True)
     pathlib.Path(images_path).mkdir(exist_ok=True)
-    book_links = parse_categorye(args.start_page, args.end_page)
+    book_links = parse_category(args.start_page, args.end_page)
+    books = []
     for book_link in book_links:
-        parsed = urlsplit(book_link)
-        book_id = os.path.split(parsed.path)[0][2:]
+        book_id = re.search(r'\d+', book_link).group(0)
         payload = {'id': book_id}
         try:
             book_page = parse_book_page(book_id)
-            if args.json_path:
-                json_path = args.json_path
-            else:
-                json_path = ''
-            pathlib.Path(json_path).mkdir(exist_ok=True)
-            filepath_json = Path(json_path) / 'books.json'
-            with open(filepath_json, 'a') as my_file:
-                json.dump(book_page, my_file, ensure_ascii=False)
             book_title = book_page['Заголовок']
             filename = f'{book_id}.{book_title}.txt'
             url = book_page['Картинка']
-            if args.skip_imgs == False:
+            if not args.skip_imgs:
                 download_image(url, book_id, images_path)
-            if args.skip_txt == False:
+            if not args.skip_txt:
                 download_txt('https://tululu.org/txt.php', filename, books_path, params=payload)
+            del book_page['Картинка']
+            book_folder = Path.cwd() / Path(books_path) / filename
+            book_page['Ссылка'] = str(book_folder)
+            books.append(book_page)
         except requests.HTTPError:
             pass
+    json_path = args.json_path or ''
+    pathlib.Path(json_path).mkdir(exist_ok=True)
+    filepath_json = Path(json_path) / 'books.json'
+    with open(filepath_json, 'w') as my_file:
+        json.dump(books, my_file, ensure_ascii=False)
+
 
 
 
